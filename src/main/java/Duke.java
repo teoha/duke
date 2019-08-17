5dfa4c9eac32f4ba3c426a449e094a6bf607d612
Derrick Teo <derrickteohaoying@gmail.com>
2019-08-18 00:23:40 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,10 +6,10 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner scanner = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
         while(scanner.hasNext()) {
             String s = scanner.nextLine();
-            switch (s){
+            switch(s.split(" ")[0]){
                 case("bye"):
                     System.out.println("Bye. Hope to see you again soon!");
                     System.exit(0);
@@ -18,9 +18,14 @@ public class Duke {
                         System.out.println((i+1)+". "+list.get(i));
                     }
                     break;
+                case("done"):
+                    int index = Integer.parseInt(s.split(" ")[1])-1;
+                    Task task = list.get(index);
+                    task.setDone(true);
+                    break;
                 default:
                     System.out.println("added: "+s);
-                    list.add(s);
+                    list.add(new Task(s));
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone=false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s",getStatusIcon(), description);
+    }
+}
